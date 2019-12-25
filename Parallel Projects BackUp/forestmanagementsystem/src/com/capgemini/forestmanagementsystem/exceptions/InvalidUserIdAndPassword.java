6b1d3a146606cbb2ccd27cf3ffde8285c6be41fa
Omkar571 <omkarnaik571@gmail.com>
2019-12-26 00:51:43 +0530

    src/com/capgemini/forestmanagementsystem/controller/AdminPage.java

---
+++ Parallel Projects BackUp/forestmanagementsystem/src/com/capgemini/forestmanagementsystem/exceptions/InvalidUserIdAndPassword.java
@@ -0,0 +1,10 @@
+package com.capgemini.forestmanagementsystem.exceptions;
+
+public class InvalidUserIdAndPassword  extends RuntimeException{
+
+	public void invalidUserIdandPassword() {
+		System.err.println("\nTHERE ARE NO CLIENTS PRESENT IN DATABASE....\n");
+	}
+		
+	
+}
